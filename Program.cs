083f09686656a99cb08a624f6608aaee4fda18b1
Kirill <shadow986@mail.ru>
2023-02-28 23:18:30 +0300

    Задание 36

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//найти сумму элементов на нечётных позициях в случайном массиве
+Console.WriteLine("Введите кол-во элементов в массиве:");
+int numbermas = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[numbermas];
+Random rnd = new Random();
+for (int count = 0; count < numbermas; count++)
+{
+    mas[count] = rnd.Next(0, 100);
+    Console.WriteLine("Элемент массива номер" + count + " = " + mas[count]);
+}
+double sum = 0;
+int count1 = 1;
+do
+{
+    sum = sum + mas[count1];
+    count1 = count1+2;
+}
+while (count1 < numbermas);
+Console.WriteLine("Сумма элементов массива на нечётных позициях = " + sum);
